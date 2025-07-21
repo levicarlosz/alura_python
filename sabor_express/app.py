dc38d7d1b155474fcbbb38a63f446db4f0974dc1
Levi Carlos <olevicarlos.dev@gmail.com>
2025-07-20 22:40:00 -0300

    adicionando funcionalidades novas ao projeto

--- sabor_express/app.py
+++ sabor_express/app.py
@@ -1,5 +1,8 @@
 import os
 
+restaurantes = []
+
+
 def encerrar():
     os.system("cls")
     print("Finalizado...")
@@ -20,25 +23,59 @@ def menu():
     print("3. Ativar Restaurante")
     print("4. Sair \n")
 
+def voltar_menu():
+    input("Pressione qualquer tecla para sair: ")
+    main()
+
+def exibir_subtitulo(texto:str):
+    os.system("cls")
+    print(f"{texto} \n")
+
+def cadastrar_restaurante():
+    exibir_subtitulo("Cadastre um Novo Restaurante:")
+    nome_restaurante = input("Nome do Restaurante: ")
+    os.system("cls")
+    restaurantes.append(nome_restaurante)
+    print(f"O restaurante {nome_restaurante} foi cadastrado com sucesso \n")
+    voltar_menu()
+
+
+def listar_restaurantes():
+    exibir_subtitulo("Restaurantes Cadastrados:")
+    for restaurante in restaurantes:
+        print(f".{restaurante}")
+    
+    os.system("cls")
+    voltar_menu()
+
+def opcao_invalida():
+    os.system("cls")
+    print("Opção Inválida! \n")
+    voltar_menu()
+
 
 def escolha_opcoes():
-    opcao_escolhida = int(input("Esolha uma Opção: "))
-    print(f"Você escolheu a opção {opcao_escolhida}!")
+    try:
+        opcao_escolhida = int(input("Esolha uma Opção: "))
+        print(f"Você escolheu a opção {opcao_escolhida}!")
 
-    match(opcao_escolhida):
-        case 1:
-            print("1. Cadastrar Restaurante")
-        case 2:
-            print("2. Listar Restaurantes")
-        case 3:
-            print("3. Ativar Restaurante")
-        case 4:
-            encerrar()
-        case _:
-            print("Opção Inválida! \n")
+        match (opcao_escolhida):
+            case 1:
+                cadastrar_restaurante()
+            case 2:
+                listar_restaurantes()
+            case 3:
+                print("3. Ativar Restaurante")
+            case 4:
+                encerrar()
+            case _:
+                opcao_invalida()
+    except:
+        opcao_invalida()
 
 
 def main():
+    os.system("cls")
     titulo()
     menu()
     escolha_opcoes()
